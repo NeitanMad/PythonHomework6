8a971ba1b7849d7f7433e86e3cc02deb0c792c2f
Azerty <Azerty@DESKTOP-HHU9R9S>
2023-09-16 23:07:59 +0300

    Done

--- PythonHomework6.py
+++ PythonHomework6.py
@@ -1,3 +1,12 @@
+from random import randint
+
+def find_indexes(list, min_value, max_value):
+    indexes = []
+    for i in range(len(list)):
+        if min_value <= list[i] <= max_value:
+            indexes.append(i)
+    return indexes
+
 def arithmetic_progression(element, difference, n):
     progression = [element + (_ - 1) * difference for _ in range(1, n + 1)]
     return progression
@@ -11,3 +20,14 @@ n = int(input("Введите количество элементов: "))
 
 some_progression = arithmetic_progression(element, difference, n)
 print(some_progression)
+
+#Задача 32
+
+list = [randint(-100, 100) for _ in range(10)]
+
+# Заданный диапазон
+min_value = 33
+max_value = 200
+
+index = find_indexes(list, min_value, max_value)
+print(index)
